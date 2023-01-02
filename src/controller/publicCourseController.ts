6bc7100e0570ad2b2ad3d9266dbb265537face1b
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-01-02 17:01:04 +0900

    ✨ feat(publiccourse): createPublicCourse 컨트롤러작성 (#7)
    #7

--- src/controller/publicCourseController.ts
+++ src/controller/publicCourseController.ts
@@ -1,7 +1,15 @@
 import { Request, Response } from "express";
 import { publicCourseService } from "../service";
+import { rm, sc } from "../constant";
+import { success,fail } from "../constant/response";
 
-const createPublicCourse = async (req: Request, res: Response) => {};
+const createPublicCourse = async (req: Request, res: Response) => {
+  const machineId = req.headers.machineId;
+
+  if(machineId){
+    res.
+  }
+};
 
 const getPublicCourseByUser = async (req: Request, res: Response) => {};
 
--- src/router/publicCourseRouter.ts
+++ src/router/publicCourseRouter.ts
@@ -5,5 +5,9 @@ import { publicCourseController } from "../controller";
 const publicCourseRouter: Router = Router();
 
 publicCourseRouter.post("/", publicCourseController.createPublicCourse);
+publicCourseRouter.get("/", publicCourseController.recommendPublicCourse);
+publicCourseRouter.get("/user", publicCourseController.getPublicCourseByUser);
+publicCourseRouter.get("/detail/:publicCourseId", publicCourseController.getPublicCourseDetail);
+publicCourseRouter.get("search", publicCourseController.searchPublicCourse);
 
 export default publicCourseRouter;
