00b0ec7aacbddae71d1dc550ee2bed9763e7591b
funnysunny08 <rosa7536@naver.com>
2023-01-02 19:21:56 +0900

    🧹 chore: path 타입 테스트 완료 (#2)
    #2

--- src/service/courseService.ts
+++ src/service/courseService.ts
@@ -5,21 +5,10 @@ const prisma = new PrismaClient();
 
 //* 코스 그리기
 const createCourse = async (courseCreateDTO: CourseCreateDTO) => {
-
-    // const data = await prisma.course.create({
-    //     data: {
-    //         user_machine_id: machineId,
-    //         departure_region: departures[0],
-    //         departure_city: departures[1],
-    //         departure_town: departures[2],
-    //         departure_detail: departures[3],
-    //         distance: courseCreateDTO.distance,
-    //         image: location,
-    //         departure_name: courseCreateDTO.departureName,
-    //         path: courseCreateDTO.path,
-    //     }
-    // });
-    const data = await prisma.$queryRaw`INSERT INTO "Course" (user_machine_id, departure_region, departure_city, departure_town, departure_detail, distance, image, departure_name, path) VALUES(${courseCreateDTO.machineId}, ${courseCreateDTO.region}, ${courseCreateDTO.city}, ${courseCreateDTO.town}, ${courseCreateDTO.detail}, ${courseCreateDTO.distance}, ${courseCreateDTO.image}, ${courseCreateDTO.name}, ${courseCreateDTO.path})`;
+    
+    const temp = '[(23.11, 12.11), (22.11, 14.11)]'; // 
+    // const data = await prisma.$queryRaw`INSERT INTO "Course" (user_machine_id, departure_region, departure_city, departure_town, departure_detail, distance, image, departure_name, path) VALUES(${courseCreateDTO.machineId}, ${courseCreateDTO.region}, ${courseCreateDTO.city}, ${courseCreateDTO.town}, ${courseCreateDTO.detail}, ${courseCreateDTO.distance}, ${courseCreateDTO.image}, ${courseCreateDTO.name}, ${courseCreateDTO.path})`;
+    const data = await prisma.$queryRaw`INSERT INTO "Course" (user_machine_id, departure_region, departure_city, departure_town, departure_detail, distance, image, departure_name, path) VALUES(${courseCreateDTO.machineId}, ${courseCreateDTO.region}, ${courseCreateDTO.city}, ${courseCreateDTO.town}, ${courseCreateDTO.detail}, ${courseCreateDTO.distance}, ${courseCreateDTO.image}, ${courseCreateDTO.name}, ${temp}::path)`;
     return data;
 };
 
