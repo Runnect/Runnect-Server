62af5906b04a18cb9a0926daccee74703094f763
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-01-03 19:13:49 +0900

    ✨ feat(publiccourse controller, service): 프리즈마 에러메시지를 이용한 400 분기처리 (#7)
    #7

--- src/controller/publicCourseController.ts
+++ src/controller/publicCourseController.ts
@@ -8,7 +8,7 @@ import { timestampConvertString } from "../module/convert/convertTime";
 
 const createPublicCourse = async (req: Request, res: Response) => {
   const error = validationResult(req);
-  //에러처리 1 : 필요한 정보(machineId,courseId, title, description이 안들어왔을때)
+  //에러처리 1 : 필요한 정보(courseId, title, description이 안들어왔을때)
   if (!error.isEmpty()) {
     const validationErrorMsg = error.errors[0].msg; //일단 이렇게두고 나중에 errorformatter 차용해서 자세히적기
     return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, validationErrorMsg));
@@ -18,11 +18,16 @@ const createPublicCourse = async (req: Request, res: Response) => {
 
   try {
     const createdPublicCourse = await publicCourseService.createPublicCourse(publicCourseCreateRequestDTO);
+
     if (!createdPublicCourse) {
-      //여기에 존재하지 않는 코스아이디, 없는 유저있으면 좋겠다. 분기처리필요
       return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.BAD_REQUEST));
+    } else if (typeof createdPublicCourse === "string" || createdPublicCourse instanceof String) {
+      console.log(createdPublicCourse);
+      return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, createdPublicCourse as string));
     } else {
+      //db에서 출력받은 timestamp를 string으로 변환
       const createdAt = timestampConvertString(createdPublicCourse.created_at);
+
       const publicCourseCreateResponseDTO: PublicCourseCreateResponseDTO = {
         publicCourse: {
           createdAt: createdAt,
@@ -32,7 +37,7 @@ const createPublicCourse = async (req: Request, res: Response) => {
       return res.status(sc.OK).send(success(sc.OK, rm.UPLOAD_PUBLIC_COURSE, publicCourseCreateResponseDTO));
     }
   } catch (error) {
-    console.log(error);
+    //console.log(error);
     //서버내부오류
     res.status(sc.INTERNAL_SERVER_ERROR).send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
   }
--- src/router/publicCourseRouter.ts
+++ src/router/publicCourseRouter.ts
@@ -7,6 +7,23 @@ const publicCourseRouter: Router = Router();
 publicCourseRouter.post(
   "/",
   [
+    body("courseId")
+      .notEmpty()
+      .withMessage("코스 아이디가 없습니다.")
+      .isNumeric()
+      .withMessage("코스 아이디가 숫자가 아닙니다."),
+    body("title")
+      .notEmpty()
+      .withMessage("제목이 없습니다."),
+    body("description")
+      .notEmpty()
+      .withMessage("설명이 없습니다."),
+  ],
+  publicCourseController.createPublicCourse
+);
+publicCourseRouter.get(
+  "/",
+  [
     header("machineId")
       .notEmpty()
       .withMessage("기기넘버가 없습니다."),
@@ -22,9 +39,8 @@ publicCourseRouter.post(
       .notEmpty()
       .withMessage("설명이 없습니다."),
   ],
-  publicCourseController.createPublicCourse
+  publicCourseController.recommendPublicCourse
 );
-publicCourseRouter.get("/", publicCourseController.recommendPublicCourse);
 publicCourseRouter.get("/user", publicCourseController.getPublicCourseByUser);
 publicCourseRouter.get("/detail/:publicCourseId", publicCourseController.getPublicCourseDetail);
 publicCourseRouter.get("search", publicCourseController.searchPublicCourse);
