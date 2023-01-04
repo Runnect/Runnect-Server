0370a05f41077bddb5964f8ea3053a19ca64cd6a
funnysunny08 <rosa7536@naver.com>
2023-01-04 16:05:55 +0900

    🧹 chore: controller 작성중 (#18)
    #18

--- src/controller/courseController.ts
+++ src/controller/courseController.ts
@@ -6,7 +6,22 @@ import { rm, sc } from '../constant';
 import { courseService } from '../service';
 import { requestConvertDeparture } from '../module/convert/requestConvertDeparture';
 
+/**
+ * @route  GET/course/user
+ * @desc 내가 그린 코스 조회 (업로드 포함)
+ * @access 
+ */
+const getCourseByUser = async (req: Request, res: Response) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.NO_USER));
 
+    const machineId = req.header("machineId");
+    try {
+        const data = await courseService.getCourseByUser(machineId);
+    }
+
+
+};
 
 
 
