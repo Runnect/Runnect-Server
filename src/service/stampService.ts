ad5febb22f832820f3f6698f8f13ca9a216170f8
funnysunny08 <rosa7536@naver.com>
2023-01-04 19:53:03 +0900

    ✨ feat: 개수 가져오기 (#23)
    #23

--- src/service/stampService.ts
+++ src/service/stampService.ts
@@ -3,8 +3,46 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const createStampByUser = async (machineId: string, option: string) => {
+    const getCounts = getCount(machineId, option);
+    
    
 
+};
+
+const getCount = async(machineId: string, option: string) => {
+    let dataCount;
+    if (option == 'course') { // 코스 그리기
+        dataCount = (await prisma.course.findMany({
+            where: {
+                AND: [{ user_machine_id: machineId }, { deleted_at: null }],
+            },
+        })).length;
+
+
+    } else if (option == 'scrap') { // 스크랩
+        dataCount = (await prisma.scrap.findMany({
+            where: {
+                user_machine_id: machineId,
+            },
+        })).length;
+
+    } else if (option == 'upload') { // 업로드
+        dataCount = (await prisma.course.findMany({
+            where: {
+                AND: [{ user_machine_id: machineId }, { deleted_at: null }, {private: false}],
+            },
+        })).length;
+
+    } else if (option == 'record') { // 달리기
+        dataCount = (await prisma.record.findMany({
+            where: {
+                AND: [{ user_machine_id: machineId }, { deleted_at: null }],
+            },
+        })).length;
+    } else {
+        return null;
+    }
+    return dataCount;
 };
 
 const stampService = {
