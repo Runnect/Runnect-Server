1b8bc09d9f41b75f9781d8178c59b5cdeb9e6f6f
funnysunny08 <rosa7536@naver.com>
2023-01-05 17:28:14 +0900

    ✨ feat: service단 에러 분기 처리 (#40)
    #40

--- src/service/userService.ts
+++ src/service/userService.ts
@@ -1,4 +1,4 @@
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { PrismaClientKnownRequestError, PrismaClientValidationError } from '@prisma/client/runtime';
 import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -13,7 +13,15 @@ const signUp = async (machineId: string, nickname: string) => {
         });
         if (!createdUser) return null;
     } catch (error) {
-        
+        if (error instanceof PrismaClientKnownRequestError) {
+            if (error.code == "P2002") {
+                // 이미 생성한 유저
+                return `이미 생성된 유저입니다.`;
+            }
+        } else if (error instanceof PrismaClientValidationError) {
+            return `${error.message}`;
+        }
+        throw error;
     }
 };
 
