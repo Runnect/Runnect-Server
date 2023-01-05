8877428aba5f2480d9eafb948eb56b783170e82a
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-01-05 14:52:56 +0900

    🏗️ build: ts 프로젝트에서 mocha를 사용하는 법 (#32)
    #32

--- test/publicCourse.spec.js
+++ test/publicCourse.spec.js
@@ -1,36 +1,47 @@
+/*const app = require("../src/index.ts");
+const request = require("supertest");
+const { expect } = require("chai");
+const dotenv = require("dotenv");
+*/
 import app from "../src/index";
-import { expect } from "chai";
-import supertest from "supertest";
+import request from "supertest";
+import expect from "chai";
 import dotenv from "dotenv";
-import { request } from "http";
-
 dotenv.config();
 
-
-
-describe("POST ~/api/public-course", ()=>{
-    it("코스 업로드 성공",(done)=>{
-        request(app)
-          .post("/api/public-course")
-          .set("Content-Type", "application/json")
-          .set("machineId", process.env.MACHINE_ID)
-          .send({
-            "courseId" : ,
-            "title" : ,
-            "description",
-          })
-          .expect(200)
-          .expect("Content-Type", /json/)
-          .then((res) => {
-            expect(res.body.data.nickname).to.equal("jobchae");
-            expect(res.body.data.totalContentNumber).to.equal(9);
-            expect(res.body.data.totalCategoryNumber).to.equal(6);
-            expect(res.body.data.totalSeenContentNumber).to.equal(1);
-            done();
-          })
-          .catch((err) => {
-            console.error("######Error >>", err);
-            done(err);
-          });
-    });
+describe("POST ~/api/public-course", () => {
+  it("코스 업로드 성공", (done) => {
+    request(app)
+      .post("/api/public-course") //url
+      .set("Content-Type", "application/json") //req.headers
+      .set("machineId", process.env.MACHINE_ID)
+      .send({
+        //request.body
+        courseId: process.env.COURSE_ID,
+        title: "mocha test1",
+        description: "mocha test1",
+      })
+      .expect(200) //예측상태코드
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        done();
+      })
+      .catch((err) => {
+        console.error("######에러 >>", err);
+        done(err);
+      });
+  });
+  it("필요한값이 없을때", (done) => {
+    request(app)
+      .post("/api/public-course") //url
+      .set("Content-Type", "application/json") //req.headers
+      .expect(400)
+      .then((res) => {
+        done();
+      })
+      .catch((err) => {
+        console.error("######에러 >>", err);
+        done(err);
+      });
+  });
 });
