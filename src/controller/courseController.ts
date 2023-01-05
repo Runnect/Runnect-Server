04284416cd86d079e09055a6f04e961339aa4192
funnysunny08 <rosa7536@naver.com>
2023-01-05 15:28:27 +0900

    ✨ feat: machineId, courseId validate 검사 추가 (#37)
    #37

--- src/controller/courseController.ts
+++ src/controller/courseController.ts
@@ -43,10 +43,35 @@ const getPrivateCourseByUser = async (req: Request, res: Response) => {
     }
 };
 
+/**
+ * @route  GET/course/detail/:courseId
+ * @desc 내가 그린 코스 상세 페이지와 달리기 기능
+ * @access 
+ */
+const getCourseDetail = async (req: Request, res: Response) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) { 
+        const validationErrorMsg = error["errors"][0].msg;
+        return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, validationErrorMsg));
+    }
+
+    const machineId = req.header("machineId") as string;
+    const { courseId } = req.params;
+
+    try {
+        const data = await courseService.getCourseDetail(machineId, courseId);
+
+    } catch (e) {
+        console.error(e);
+        res.status(sc.INTERNAL_SERVER_ERROR).send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+    }
+};
+
 
 const courseController = {
     getCourseByUser,
     getPrivateCourseByUser,
+    getCourseDetail,
 };
 
 export default courseController;
