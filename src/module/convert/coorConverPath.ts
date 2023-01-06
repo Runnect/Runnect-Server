0b2d0b32f9f4ebf054b2ad83d2d8f38823b3f09b
RInRInPark <surinp8019@naver.com>
2023-01-06 14:47:57 +0900

    🧹 chore(coorconvertpath): 오타 수정, request로 path객체가 온다고 가 (#5)
    #5

--- src/module/convert/coorConverPath.ts
+++ src/module/convert/coorConverPath.ts
@@ -1,8 +1,7 @@
 // { "path" : [{"lat" : "23.333", "long" : "45.444"},{"lat" : "23.22", "long" : "45.222"}]}의 형태로 req
 
-export const coorConverPath = (coor: any) => {
-  const coorArray = coor["path"];
-  const pathArray: string[] = coorArray.map((element: any) => {
+export const coorConvertPath = (coor: any) => {
+  const pathArray: string[] = coor.map((element: any) => {
     return [element.lat * 1, element.long * 1]; //string을 number로 바꾸기 위해 *1
   });
   // pathArray는 [[23.333,45.444],[23.22,45.222]] (이중배열 모양)
