54cbe7a5c209dae343dc3c4e479d51cfdbc35cfc
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-01-08 15:44:41 +0900

    ✨ feat: get stamp by user 구현 완료 (n)
    n

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import courseRouter from "./courseRouter";
 import recordRouter from "./recordRouter";
 import userRouter from "./userRouter";
 import scrapRouter from "./scrapRouter";
+import stampRouter from "./stampRouter";
 
 const router: Router = Router();
 
@@ -12,5 +13,6 @@ router.use("/course", courseRouter);
 router.use("/record", recordRouter);
 router.use("/user", userRouter);
 router.use("/scrap", scrapRouter);
+router.use("/stamp", stampRouter);
 
 export default router;
