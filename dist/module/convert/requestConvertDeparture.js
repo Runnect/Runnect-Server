02b729a0007da85393cd578171fccd5f4c8bafbf
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-01-10 14:30:17 +0900

    🐛 fix(주소convert모듈): 상세주소는 띄어쓰기 있어도 되도록

--- dist/module/convert/requestConvertDeparture.js
+++ dist/module/convert/requestConvertDeparture.js
@@ -3,14 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.requestConvertDeparture = void 0;
 const requestConvertDeparture = (departureAddress, departureName) => {
     const departures = departureAddress.split(" ");
-    if (departures.length == 4) {
-        return {
-            region: departures[0],
-            city: departures[1],
-            town: departures[2],
-            detail: departures[3],
-            name: departureName,
-        };
+    if (departures.length < 3) {
+        return null;
     }
     else if (departures.length == 3) {
         return {
@@ -20,8 +14,25 @@ const requestConvertDeparture = (departureAddress, departureName) => {
             name: departureName,
         };
     }
-    else
-        return null;
+    else if (departures.length == 4) {
+        return {
+            region: departures[0],
+            city: departures[1],
+            town: departures[2],
+            detail: departures[3],
+            name: departureName,
+        };
+    }
+    else {
+        //departures length 가 4이상인경우
+        return {
+            region: departures[0],
+            city: departures[1],
+            town: departures[2],
+            detail: departures.slice(3, departures.length).join(" "),
+            name: departureName,
+        };
+    }
 };
 exports.requestConvertDeparture = requestConvertDeparture;
 //# sourceMappingURL=requestConvertDeparture.js.map
--- src/module/convertDataForm.ts
+++
@@ -1,9 +0,0 @@
-// src/modules/convertDataForm.ts
-
-import dayjs from "dayjs";
-
-export const convertDateForm = (date: Date): string => {
-  const now: string = dayjs(date).format("YYYY.MM.DD");
-
-  return now;
-};
