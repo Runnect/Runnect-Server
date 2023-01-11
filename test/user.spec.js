297196f21de6744fc6ef916011e41e7e182bb6e6
funnysunny08 <rosa7536@naver.com>
2023-01-11 13:28:52 +0900

    ✨ feat: 회원가입 테스트 코드 (#74)
    #74

---
+++ test/user.spec.js
@@ -0,0 +1,41 @@
+import app from "../src/index";
+import request from "supertest";
+import expect from "chai";
+import dotenv from "dotenv";
+dotenv.config();
+
+//* signUp
+describe("POST ~/api/user", () => {
+    it("회원 가입 성공", (done) => {
+      request(app)
+        .post("/api/user") //url
+        .set("Content-Type", "application/json") //req.headers
+        .set("machineId", "spongebob2")
+        .send({
+          //request.body
+          nickname: "스폰지밥2"
+        })
+        .expect(200) //예측상태코드
+        .expect("Content-Type", /json/)
+        .then((res) => {
+          done();
+        })
+        .catch((err) => {
+          console.error("######에러 >>", err);
+          done(err);
+        });
+    });
+    it("필요한값이 없을때", (done) => {
+      request(app)
+        .post("/api/user") //url
+        .set("Content-Type", "application/json") //req.headers
+        .expect(400)
+        .then((res) => {
+          done();
+        })
+        .catch((err) => {
+          console.error("######에러 >>", err);
+          done(err);
+        });
+    });
+  });
