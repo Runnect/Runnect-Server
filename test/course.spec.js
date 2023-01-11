39e1bfb6e331bd795384da52d7bfd16c3b6d956a
funnysunny08 <rosa7536@naver.com>
2023-01-11 12:22:01 +0900

    ✨ feat: getPrivateCourseByUser test 작성 (#68)
    #68

--- test/course.spec.js
+++ test/course.spec.js
@@ -5,6 +5,38 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
+//* getPrivateCourseByUser
+describe("GET ~/api/course/private/user", () => {
+    it("private 코스 조회 성공", (done) => {
+      request(app)
+        .get("/api/course/private/user") //url
+        .set("Content-Type", "application/json") //req.headers
+        .set("machineId", process.env.MACHINE_ID)
+        .expect(200) //예측상태코드
+        .expect("Content-Type", /json/)
+        .then((res) => {
+          done();
+        })
+        .catch((err) => {
+          console.error("######에러 >>", err);
+          done(err);
+        });
+    });
+    it("필요한값이 없을때", (done) => {
+      request(app)
+        .get("/api/course/private/user") //url
+        .set("Content-Type", "application/json") //req.headers
+        .expect(400)
+        .then((res) => {
+          done();
+        })
+        .catch((err) => {
+          console.error("######에러 >>", err);
+          done(err);
+        });
+    });
+  });
+
 //* getCourseDetail
 describe("GET ~/api/course/detail/:courseId", () => {
     it("코스 조회 성공", (done) => {
