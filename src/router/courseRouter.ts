83ea908348ae6cadbafcd5324261a17afda59e2b
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-01-13 22:42:00 +0900

    🧹 chore: 안드를 위한 코스 그리기 req consol log

--- src/router/courseRouter.ts
+++ src/router/courseRouter.ts
@@ -1,4 +1,6 @@
-import { Router } from "express";
+//import { Router } from "express";
+import { Router, Request, Response, NextFunction } from "express";
+
 import { courseController } from "../controller";
 import { multiformDataConvert, upload } from "../middleware";
 import { header, body, param } from "express-validator";
@@ -7,23 +9,19 @@ const router: Router = Router();
 
 router.post(
   "/",
+
+  (req: Request, res: Response, next: NextFunction) => {
+    console.log(req);
+    next();
+  },
+
   upload.single("image"),
   multiformDataConvert,
   [
-    header("machineId")
-      .notEmpty()
-      .withMessage("기기넘버가 없습니다"),
-    body("path")
-      .notEmpty()
-      .withMessage("경로가 없습니다."),
-    body("distance")
-      .notEmpty()
-      .withMessage("거리 정보가 없습니다.")
-      .isNumeric()
-      .withMessage("거리 정보가 숫자가 아닙니다."),
-    body("departureAddress")
-      .notEmpty()
-      .withMessage("출발지 정보가 없습니다."),
+    header("machineId").notEmpty().withMessage("기기넘버가 없습니다"),
+    body("path").notEmpty().withMessage("경로가 없습니다."),
+    body("distance").notEmpty().withMessage("거리 정보가 없습니다.").isNumeric().withMessage("거리 정보가 숫자가 아닙니다."),
+    body("departureAddress").notEmpty().withMessage("출발지 정보가 없습니다."),
   ],
   courseController.createCourse
 );
@@ -37,14 +35,8 @@ router.get("/private/user", [header("machineId").notEmpty()], courseController.g
 router.get(
   "/detail/:courseId",
   [
-    header("machineId")
-      .notEmpty()
-      .withMessage("기기넘버가 없습니다."),
-    param("courseId")
-      .notEmpty()
-      .withMessage("코스 아이디가 없습니다.")
-      .isNumeric()
-      .withMessage("코스 아이디가 숫자가 아닙니다."),
+    header("machineId").notEmpty().withMessage("기기넘버가 없습니다."),
+    param("courseId").notEmpty().withMessage("코스 아이디가 없습니다.").isNumeric().withMessage("코스 아이디가 숫자가 아닙니다."),
   ],
   courseController.getCourseDetail
 );
