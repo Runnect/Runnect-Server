92ce03971f46bcff91b2ebf7e88cc6c4f92c2474
funnysunny08 <rosa7536@naver.com>
2023-02-28 21:07:27 +0900

    ✨ feat: googleStrategy  작성중 (#91)
    #91

---
+++ src/module/passport/googleStrategy.ts
@@ -0,0 +1,45 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+module.exports = () => {
+    passport.use(
+        new GoogleStrategy(
+            {
+                clientID: process.env.GOOGLE_ID, // 구글 로그인에서 발급받은 REST API 키
+                clientSecret: process.env.GOOGLE_SECRET,
+                callbackURL: 'api/user/google/callback', // 구글 로그인 Redirect URI 경로
+            },
+            async(accessToken: any, refreshToken: any, profile: any, done: any) => {
+                console.log('google profile: ', profile);
+                try {
+                    const exUser = prisma.user.findUnique({
+                        where: {
+                            id: profile.id,
+                            provider: 'google',
+                        }
+                    });
+                    if (exUser) {
+                        done(null, exUser);
+                    } else {
+                        const newUser = prisma.user.create({
+                            data: {
+                                id: profile.id,
+                                provider: 'google',
+                                email: profile?.email[0].value,
+                                nickname:
+                            }
+                        });
+                        done(null, newUser);
+                    }
+                } catch (error) {
+                    console.error(error);
+                    done(error);
+                }
+            }
+        )
+    )
+};
