6e6b84ff1f31d398d906dc9eaa7e32dfa1f28e2a
funnysunny08 <rosa7536@naver.com>
2023-03-17 16:55:09 +0900

    ✨ feat: social.ts 구글 소셜로그인 부분 작성 (#95)
    #95

---
+++ src/module/social.ts
@@ -0,0 +1,31 @@
+const {OAuth2Client} = require('google-auth-library');
+
+const google = async (idToken: string) => {
+    try {
+        const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+        const verify = async () => {
+            const ticket = await client.verifyIdToken({
+                idToken: idToken,
+                audience: process.env.GOOGLE_CLIENT_ID,
+            });
+            const payload = ticket.getPayload();
+
+            const userid = payload['sub'];
+            const email = payload.getEmail();
+
+            return email; //* return 값 정하기
+        };
+        verify().catch(console.error);
+
+    } catch (error) {
+        console.log(error);
+
+    }
+};
+
+const social = {
+    google,
+};
+
+export default social;
