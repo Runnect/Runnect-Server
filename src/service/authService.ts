2c324c36818ca096b03e25d283ab3b12df1f7ddb
funnysunny08 <rosa7536@naver.com>
2023-03-19 14:32:19 +0900

    ✨ feat: authService에서 createUser 에러 처리 (#95)
    #95

--- src/service/authService.ts
+++ src/service/authService.ts
@@ -1,11 +1,13 @@
 import { SocialCreateRequestDTO } from './../interface/DTO/auth/SocialCreateDTO';
 import { PrismaClient } from '@prisma/client';
+import { randomInitialNickname } from '../module/randomInitialNickname';
+import { PrismaClientKnownRequestError, PrismaClientValidationError } from "@prisma/client/runtime";
 
 const prisma = new PrismaClient();
 
 const getUserByEmail = async (socialCreateRequestDTO: SocialCreateRequestDTO) => {
     try {
-        const userByEmail = await prisma.user.findMany({
+        const userByEmail = await prisma.user.findFirst({
             where: {
                 AND: [{ email: socialCreateRequestDTO.email }, { provider: socialCreateRequestDTO.provider }],
             },
@@ -17,12 +19,11 @@ const getUserByEmail = async (socialCreateRequestDTO: SocialCreateRequestDTO) =>
     }
 };
 
-
 const createUser = async (socialCreateRequestDTO: SocialCreateRequestDTO) => {
     try {
         const newUser = await prisma.user.create({
             data: {
-                nickname: "바꾸기바꾸기",
+                nickname: randomInitialNickname(),
                 social_id: socialCreateRequestDTO.socialId,
                 email: socialCreateRequestDTO.email,
                 provider: socialCreateRequestDTO.provider,
@@ -30,7 +31,15 @@ const createUser = async (socialCreateRequestDTO: SocialCreateRequestDTO) => {
         });
         return newUser;
     } catch (error) {
-        
+        if (error instanceof PrismaClientKnownRequestError) {
+            if (error.code == "P2002") {
+                // 이미 생성한 유저
+                return `이미 생성된 유저입니다.`;
+            }
+        } else if (error instanceof PrismaClientValidationError) {
+            return `${error.message}`;
+        }
+        throw error;
     }
 };
 
