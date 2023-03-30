d65206948a10c7537b1935cac761a2b60a928e8d
sunny <rosa7536@naver.com>
2023-03-30 23:02:26 +0900

    ✨ feat: deletePublicCourse controller 작성중 (#126)
    #126

--- src/controller/publicCourseController.ts
+++ src/controller/publicCourseController.ts
@@ -218,12 +218,31 @@ const searchPublicCourse = async (req: Request, res: Response) => {
   }
 };
 
+const deletePublicCourse = async (req: Request, res: Response) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    const validationErrorMsg = error["errors"][0].msg;
+    return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, validationErrorMsg));
+  }
+
+  const { publicCourseId } = req.params;
+
+  try {
+
+  } catch (e) {
+    console.error(e);
+    res.status(sc.INTERNAL_SERVER_ERROR).send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+  }
+
+};
+
 const publicCourseController = {
   createPublicCourse,
   getPublicCourseByUser,
   getPublicCourseDetail,
   recommendPublicCourse,
   searchPublicCourse,
+  deletePublicCourse,
 };
 
 export default publicCourseController;
