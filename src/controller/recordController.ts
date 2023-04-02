5a505a8d3aab336a4bfb72c9dd6b590f684cdea6
sunny <rosa7536@naver.com>
2023-04-02 14:07:19 +0900

    ✨ feat: 기록 삭제 컨트롤러 작성중 (#134)
    #134

--- src/controller/recordController.ts
+++ src/controller/recordController.ts
@@ -88,5 +88,21 @@ const getRecordByUser = async (req: Request, res: Response) => {
   }
 };
 
-const recordController = { createRecord, getRecordByUser };
+const deleteRecord = async (req: Request, res: Response) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    const validationErrorMsg = error["errors"][0].msg;
+    return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, validationErrorMsg));
+  }
+  const { recordId } = req.params;
+
+  try {
+
+  } catch (error) {
+    console.log(error);
+    res.status(sc.INTERNAL_SERVER_ERROR).send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+  }
+};
+
+const recordController = { createRecord, getRecordByUser, deleteRecord };
 export default recordController;
