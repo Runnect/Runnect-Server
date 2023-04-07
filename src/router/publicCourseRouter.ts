f56aa184c7bba8f20fbf4a50429138c7696d44d1
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-04-07 23:07:31 +0900

    ✨ feat: express validator를 이용해 publicCourseIdList의 Array 여부, 내부 숫자여부 확ㅇ니

--- src/router/publicCourseRouter.ts
+++ src/router/publicCourseRouter.ts
@@ -101,6 +101,11 @@ publicCourseRouter.put(
     body("publicCourseIdList")
       .notEmpty()
       .withMessage("퍼블릭 코스 아이디가 없습니다.")
+      .isArray()
+      .withMessage("퍼블릭 코스들이 리스트 형식이 아닙니다"),
+    body("publicCourseIdList.*")
+      .isNumeric()
+      .withMessage("퍼블릭 코스들가 숫자가 아닙니다."),
   ],
   publicCourseController.deletePublicCourse
 );
